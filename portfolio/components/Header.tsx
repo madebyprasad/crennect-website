31712cb61e615340d4435ae0fee776d49a301538
Prasad Dani <psdani2k@gmail.com>
2026-07-17 15:23:30 +0530

    replacing genai with app hub

--- portfolio/components/Header.tsx
+++ portfolio/components/Header.tsx
@@ -66,7 +66,7 @@ export default function Header() {
           <nav className="nav">
             <a href="/about.html" style={navLinkStyle}>About Us</a>
             <a href="https://portfolio.crennect.com/" style={navLinkStyle}>Case Studies</a>
-            <a href="https://www.crennect.com/genai" style={navLinkStyle}>GenAI</a>
+            <a href="https://www.crennect.com/app-hub" style={navLinkStyle}>GenAI</a>
             <a href="/services.html" style={navLinkStyle}>Services</a>
             <a href="#resources" style={navLinkStyle}>Resources</a>
             <a href="contact.html" style={navLinkStyle}>Contact Us</a>
